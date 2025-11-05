359d257dd2ef3af7030ed781460c114607e017a6
Timur-Basirov <153905042+Timur-Basirov@users.noreply.github.com>
2025-11-05 08:08:06 +0200

    Create PlayerRecord.cs

---
+++ PlayerRecord.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UusimangTARpv24
+{
+    class PlayerRecord
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Score { get; set; }
+    }
+}
