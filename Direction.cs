7207ca4802818854ecbc9b9e9cac4aaabb131965
Timur-Basirov <153905042+Timur-Basirov@users.noreply.github.com>
2025-11-05 08:05:17 +0200

    Update Direction.cs

--- Direction.cs
+++ Direction.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Snake
+namespace UusimangTARpv24
 {
     enum Direction
     {
